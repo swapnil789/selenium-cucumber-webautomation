85fed6bdd38a420ec0bc3302ffc45acbcd29292f
Swapnil Khute <swapnilkhute@gmail.com>
2020-09-23 11:21:53 +0530

    Add code for getting email recipient
    Add code for getting email recipient from config file

--- src/main/java/com/automation/dataproviders/ConfigFileReader.java
+++ src/main/java/com/automation/dataproviders/ConfigFileReader.java
@@ -96,5 +96,12 @@ public class ConfigFileReader {
 		 String reportConfigPath = properties.getProperty("reportConfigPath");
 		 if(reportConfigPath!= null) return reportConfigPath;
 		 else throw new RuntimeException("Report Config Path not specified in the Configuration.properties file for the Key:reportConfigPath"); 
-		}
+	}
+	
+	public String getEmailRecepients(){
+		String emailRecepients = properties.getProperty("emailRecepients");
+		if(emailRecepients!=null)
+			return emailRecepients;
+		else throw new RuntimeException("No recipients added for sending mail");
+	}
 }
